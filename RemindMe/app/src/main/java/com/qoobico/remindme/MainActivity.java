4bbd58589a9e4ebb9c19025d3122174abf3b55c1
MariyaSaprykina <marilia@inbox.ru>
2018-05-04 16:36:57 +0200

    Added Toolbar and Search Button

--- RemindMe/app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -1,13 +1,39 @@
 package com.qoobico.remindme;
 
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
     }
+
+    //@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+
+    private void initToolbar(){
+
+            toolbar =  findViewById(R.id.toolbar);
+
+            toolbar.setTitle(R.string.app_name);
+            toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    return false;
+                }
+            });
+            toolbar.inflateMenu(R.menu.menu);
+
+        }
+
 }
